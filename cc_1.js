f21db68ac8e02598f773e9af4c2a8b906ffd3053
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-08-31 10:27:10 -0500

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,4 @@
-// Task 1 Variables and Data Types
+// Task 1: Variables and Data Types
 
 let employeeName = "Jack";
 const employeeID = 12;
@@ -7,3 +7,14 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+// Task 2: Compound Data Types
+
+let products = ["shirt", "shoes", "shorts"];
+const productDetails = {
+    name: Jordans,
+    price: 150,
+    inStock: Yes,
+}
+
+console.log(products, productDetails);
