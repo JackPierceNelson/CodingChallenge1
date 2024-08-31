6d6a1e4c12db2c8598053f22219467bc19fa6ea5
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-08-31 15:56:35 -0500

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -18,3 +18,26 @@ const productDetails = {
 }
 
 console.log(products, productDetails);
+
+// Task 3: Assignment Operators
+
+let accountBalance = 3000; // Initial Account Balance consists of 3000
+
+console.log("Initial Account Balance:", accountBalance); // Output: Initial Account Balance: 3000
+
+// Updating the values of the Account Balance.
+
+accountBalance += 5; // accountBalance = acountBalance + 5
+console.log("Updated Account Balance:", accountBalance); // Output: Updated Account Balance = 3005
+
+accountBalance -= 4; // accountBalance = accountBalance -5 
+console.log("Updated Account Balance:", accountBalance); // Output: Updated Account Balance = 3001
+
+accountBalance *= 1.5; // accountBalance = acountBalance * 1.5
+console.log("Updated Account Balance:", accountBalance); // Output: Updated Account Balance = 4501.5
+
+accountBalance /= 10; // accountBalance = acountBalance / 10
+console.log("Updated Account Balance:", accountBalance); // Output: Updated Account Balance = 450.15
+
+accountBalance %= 10; // accountBalance = acountBalance modulus 10 
+console.log("Updated Account Balance:", accountBalance); // Output: Updated Account Balance = 0.14999999999997726
